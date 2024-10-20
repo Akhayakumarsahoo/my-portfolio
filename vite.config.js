7693d72357d61eec8fe84e8d057971c976dcca77
Akhaya <akhayakumarsahoo06@gmail.com>
2024-10-20 12:35:28 +0530

    optimization

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,57 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import viteImagemin from "vite-plugin-imagemin";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      mozjpeg: {
+        quality: 80,
+      },
+      pngquant: {
+        quality: [0.65, 0.9],
+        speed: 4,
+      },
+      svgo: {
+        plugins: [
+          {
+            name: "removeViewBox",
+          },
+          {
+            name: "removeEmptyAttrs",
+            active: false,
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    target: "esnext", // Use modern JS syntax
+    minify: "terser", // Use Terser for minification (better compression)
+    cssCodeSplit: true, // Enable CSS code splitting
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Split vendor modules into separate chunks
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
     },
   },
 });
