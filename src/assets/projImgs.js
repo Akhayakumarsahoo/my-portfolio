bd82c9b227c8853b08fb51adbbec59f73237256c
Akhaya <akhayakumarsahoo06@gmail.com>
2024-10-20 18:41:34 +0530

    image updt

--- src/assets/projImgs.js
+++ src/assets/projImgs.js
@@ -1,25 +1,25 @@
 export const projImgs = [
   {
     title: "Wanderlust",
-    src: "projects/wanderlust.png",
+    src: "/projects/wanderlust.png",
     desc: "A full stack Travel room booking app",
     link: " https://wanderlust-bx0h.onrender.com/listings",
   },
   {
     title: "Zerodha",
-    src: "projects/zerodhaFront.png",
+    src: "/projects/zerodhaFront.png",
     desc: "A full stack stock trading app",
     link: "https://zerodha-akhi.vercel.app",
   },
   {
     title: "A simple Calculater",
-    src: "projects/calculator.png",
+    src: "/projects/calculator.png",
     desc: "A simple calculater",
     link: "https://akhicalculater.netlify.app",
   },
   {
     title: "Simon says Game",
-    src: "projects/simonSaysGame.png",
+    src: "/projects/simonSaysGame.png",
     desc: "A simple game web app",
     link: "https://simonsaysgameakhi.vercel.app",
   },
